1ea0f255d48c1c01c5e77afd025f68e1e6a28c98
ChuangStephanie <stephanie231951@yahoo.com>
2023-08-10 20:21:12 -0500

    change Nav to Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,13 @@ import { Route, Routes, Link } from "react-router-dom";
 import Blue from "./components/Blue";
 import Red from "./components/Red";
 import Home from "./components/Home";
-import Nav from "./components/Nav";
+import NavBar from "./components/Nav";
 
 function App() {
   return (
     <>
       <div id="container">
-        <Nav />
+        <NavBar />
         <div id="main-section">
           <Routes>
             <Route path="/" element={<Home />} />
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 
-export default function Nav() {
+export default function NavBar() {
   return (
     <div id="navbar">
       <Link to="/">Home</Link>
